763f6434f6f8ed9ba5fff51b865d846be50080d3
Andrey Popovich <lessshealth@gmail.com>
2024-12-18 23:32:03 +1000

    octagon bot mysql

--- bot.js
+++ bot.js
@@ -1,28 +1,72 @@
 const TelegramBot = require('node-telegram-bot-api');
+const mysql = require('mysql2');
+
 
 const token = '7207562918:AAFSDKbYDsQWtSY4-iMjI0yLTIgLxeHKBcI';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/help/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(
-        chatId,
-        `Список доступных команд:
-/help - список всех команд
-/site - ссылка на сайт Октагона
-/creator - ФИО создателя`
-    );
-});
 
-bot.onText(/\/site/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Ссылка на сайт Октагона: https://students.forus.ru/');
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chatbottests'
 });
 
 
-bot.onText(/\/creator/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Попович Андрей Александрович');
+bot.onText(/\/randomItem/, (msg) => {
+  const chatId = msg.chat.id;
+  const query = 'SELECT * FROM items ORDER BY RAND() LIMIT 1';
+
+  db.query(query, (err, results) => {
+    if (err || results.length === 0) {
+      bot.sendMessage(chatId, 'Ошибка: не удалось получить случайный предмет.');
+    } else {
+      const item = results[0];
+      bot.sendMessage(chatId, `(${item.id}) - ${item.name}: ${item.descr}`);
+    }
+  });
 });
 
-console.log('Бот запущен!');
+
+bot.onText(/\/deleteItem (\d+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const itemId = parseInt(match[1], 10);
+  const query = 'DELETE FROM items WHERE id = ?';
+
+  db.query(query, [itemId], (err, results) => {
+    if (err || results.affectedRows === 0) {
+      bot.sendMessage(chatId, 'Ошибка: предмет не найден.');
+    } else {
+      bot.sendMessage(chatId, 'Успешно: предмет удалён.');
+    }
+  });
+});
+
+
+bot.onText(/\/getItemByID (\d+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const itemId = parseInt(match[1], 10);
+  const query = 'SELECT * FROM items WHERE id = ?';
+
+  db.query(query, [itemId], (err, results) => {
+    if (err || results.length === 0) {
+      bot.sendMessage(chatId, 'Ошибка: предмет не найден.');
+    } else {
+      const item = results[0];
+      bot.sendMessage(chatId, `(${item.id}) - ${item.name}: ${item.descr}`);
+    }
+  });
+});
+
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const commands = `
+Доступные команды:
+/randomItem - получить случайный предмет
+/deleteItem ID - удалить предмет по ID
+/getItemByID ID - получить предмет по ID
+  `;
+  bot.sendMessage(chatId, `Привет, октагон! ${commands}`);
+});
