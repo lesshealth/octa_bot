6901837b29c833ec3e3497a8050c7a7591482771
Andrey Popovich <lessshealth@gmail.com>
2024-12-18 22:25:32 +1000

    octagon bot commands added

--- bot.js
+++ bot.js
@@ -1,12 +1,28 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '7207562918:AAFSDKbYDsQWtSY4-iMjI0yLTIgLxeHKBcI';
-
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
+bot.onText(/\/help/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привет, октагон!');
+    bot.sendMessage(
+        chatId,
+        `Список доступных команд:
+/help - список всех команд
+/site - ссылка на сайт Октагона
+/creator - ФИО создателя`
+    );
 });
 
-console.log('Бот запущен');
+bot.onText(/\/site/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Ссылка на сайт Октагона: https://students.forus.ru/');
+});
+
+
+bot.onText(/\/creator/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Попович Андрей Александрович');
+});
+
+console.log('Бот запущен!');
